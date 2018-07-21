0d10c55bb382e6aef5d145fb5cf3e6e9b405c453
Alfredo Faria <alfredo.faria88@gmail.com>
2018-07-21 01:11:01 +0000

    cenas

--- src/main/java/org/academiadecodigo/hackathon/controller/webController/ProfileController.java
+++ src/main/java/org/academiadecodigo/hackathon/controller/webController/ProfileController.java
@@ -1,4 +1,10 @@
 package org.academiadecodigo.hackathon.controller.webController;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("profile")
 public class ProfileController {
+
 }
